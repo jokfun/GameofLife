4dfbda67453b7380010ee042069338cc12fe0beb
jokfun <31824574+jokfun@users.noreply.github.com>
2019-06-01 11:44:11 +0200

    Update tovideo.py

--- tovideo.py
+++ tovideo.py
@@ -8,7 +8,7 @@ images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
 frame = cv2.imread(os.path.join(image_folder, images[0]))
 height, width, layers = frame.shape
 
-video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc('Y', 'V', '1', '2'), 10, (width,height))
+video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'), 10, (width,height))
 
 for image in images:
     video.write(cv2.imread(os.path.join(image_folder, image)))
